9f54cec6f96875abaf52be92fad5e3817768f37f
Lambros Hasapis <lamberbass@gmail.com>
2022-09-04 23:02:09 +0300

    Add methods for addLiquidity and swapExactTokensForTokens. Add methods to get token balances of current account and mint tokens with zero balances. Add buttons that call these methods with hardcoded parameters.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { getCurrentAccount } from './services/web3.service';
+import { addLiquidity, getCurrentAccount, getTokenBalances, mintTokensWithZeroBalance, swapExactTokensForTokens } from './services/web3.service';
+import Tokens from './artifacts/deployed-tokens.json';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState('');
@@ -12,6 +13,48 @@ function App() {
 
   useEffect(() => { connectWallet(); }, []);
 
+  const addLiq = async () => {
+    const tokenA: string = Tokens.LINK;
+    const tokenB: string = Tokens.SHIB;
+
+    const amountTokenA: string = web3.utils.toWei('100', 'ether');
+    const amountTokenB: string = web3.utils.toWei('100', 'ether');
+    const minAmountTokenA: string = web3.utils.toWei('1', 'ether');
+    const minAmountTokenB: string = web3.utils.toWei('1', 'ether');
+
+    await addLiquidity(
+      tokenA,
+      tokenB,
+      amountTokenA,
+      amountTokenB,
+      minAmountTokenA,
+      minAmountTokenB
+    );
+  };
+
+  const swap = async () => {
+    const tokenA: string = Tokens.LINK;
+    const tokenB: string = Tokens.SHIB;
+
+    const amountTokenA: string = web3.utils.toWei('10', 'ether');
+    const minAmountTokenB: string = web3.utils.toWei('1', 'ether');
+
+    await swapExactTokensForTokens(
+      tokenA,
+      tokenB,
+      amountTokenA,
+      minAmountTokenB
+    );
+  };
+
+  const getBalances = async () => {
+    console.log('balances', await getTokenBalances());
+  };
+
+  const mint = async () => {
+    await mintTokensWithZeroBalance();
+  };
+
   return (
     <div>
       <header className="App-header">
@@ -21,6 +64,13 @@ function App() {
           ? <div>Connecting to Metamask wallet...</div>
           : <div>Selected Account: <span>{currentAccount}</span></div>
         }
+
+        <div className="App-buttons">
+          <button type="button" onClick={getBalances}>Get balances</button>
+          <button type="button" onClick={mint}>Mint tokens with zero balance</button>
+          <button type="button" onClick={addLiq}>Add liquidity</button>
+          <button type="button" onClick={swap}>Swap exact tokens for tokens</button>
+        </div>
 
         <img src="mule.svg" className="App-logo" alt="logo" />
       </header>
--- src/services/web3.service.ts
+++ src/services/web3.service.ts
@@ -1,7 +1,15 @@
 import Web3 from 'web3';
+import Router from '../artifacts/Router.json';
+import ERC20Mock from '../artifacts/ERC20Mock.json';
+import Tokens from '../artifacts/deployed-tokens.json';
 
 let web3: Web3;
 let currentAccount: string;
+let routerContract: any;
+
+type TokenAddresses = { [token: string]: string };
+export type TokenBalances = { [token: string]: string };
+export type AddLiquidityResponse = { amountA: string, amountB: string, liquidity: string };
 
 export async function getCurrentAccount(): Promise<string> {
   const ethereum = (window as any).ethereum;
@@ -20,3 +28,133 @@ export async function getCurrentAccount(): Promise<string> {
   currentAccount = accounts[0];
   return currentAccount;
 };
+
+export async function addLiquidity(
+  tokenA: string,
+  tokenB: string,
+  amountTokenA: string,
+  amountTokenB: string,
+  minAmountTokenA: string,
+  minAmountTokenB: string
+): Promise<AddLiquidityResponse> {
+  if (!routerContract) {
+    await initRouterContract();
+  }
+
+  const contractTokenA = getContractOfToken(tokenA);
+  const contractTokenB = getContractOfToken(tokenB);
+
+  await contractTokenA.methods.approve(routerContract._address, amountTokenA).send({ from: currentAccount });
+  await contractTokenB.methods.approve(routerContract._address, amountTokenB).send({ from: currentAccount });
+
+  const contractMethod = routerContract.methods.addLiquidity(
+    tokenA,
+    tokenB,
+    amountTokenA,
+    amountTokenB,
+    minAmountTokenA,
+    minAmountTokenB,
+    currentAccount
+  );
+
+  const response: AddLiquidityResponse = await contractMethod.call({ from: currentAccount });
+  console.log('addLiquidity response', response);
+
+  const receipt = await contractMethod.send({ from: currentAccount });
+  console.log('addLiquidity receipt', receipt);
+
+  return response;
+}
+
+export async function swapExactTokensForTokens(
+  tokenA: string,
+  tokenB: string,
+  amountTokenA: string,
+  minAmountTokenB: string
+): Promise<string[]> {
+  if (!routerContract) {
+    await initRouterContract();
+  }
+
+  const contractTokenA = getContractOfToken(tokenA);
+  await contractTokenA.methods.approve(routerContract._address, amountTokenA).send({ from: currentAccount });
+
+  const contractMethod = routerContract.methods.swapExactTokensForTokens(
+    amountTokenA,
+    minAmountTokenB,
+    [tokenA, tokenB],
+    currentAccount
+  );
+
+  const response: string[] = await contractMethod.call({ from: currentAccount });
+  console.log('swapExactTokensForTokens response', response);
+
+  const receipt = await contractMethod.send({ from: currentAccount });
+  console.log('swapExactTokensForTokens receipt', receipt);
+
+  return response;
+}
+
+export async function mintTokensWithZeroBalance(): Promise<void> {
+  let balances: TokenBalances = await getTokenBalances();
+  console.log('balances before minting', balances);
+
+  const amountToMint: string = web3.utils.toWei('1000', 'ether');
+  const tokenAddresses: TokenAddresses = Tokens;
+
+  const addressesToMint: string[] = Object.keys(tokenAddresses)
+    .filter((token: string) => web3.utils.toBN(balances[token]).isZero())
+    .map((token: string) => tokenAddresses[token]);
+
+  if (addressesToMint.length === 0) {
+    return;
+  }
+
+  await mintTokens(amountToMint, addressesToMint);
+
+  balances = await getTokenBalances();
+  console.log('balances after minting', balances);
+}
+
+export async function getTokenBalances(): Promise<TokenBalances> {
+  const tokenAddresses: TokenAddresses = Tokens;
+  const tokens: string[] = Object.keys(tokenAddresses);
+
+  const promises: Array<Promise<string>> = tokens.map((token: string) => {
+    const contract = getContractOfToken(tokenAddresses[token]);
+    return contract.methods.balanceOf(currentAccount).call({ from: currentAccount });
+  });
+
+  const balancesArray: string[] = await Promise.all(promises);
+
+  return balancesArray.reduce(
+    (result, balance, index) => {
+      const token: string = tokens[index];
+      return { ...result, [token]: balance }
+    },
+    {} as { [token: string]: string }
+  );
+}
+
+async function initRouterContract(): Promise<void> {
+  const networkId: number = await web3.eth.net.getId();
+  const routerInstance = (Router as any).networks[networkId];
+  if (!routerInstance) {
+    throw new Error('Router contract not deployed to detected network.');
+  }
+
+  routerContract = new web3.eth.Contract((Router as any).abi, routerInstance.address);
+}
+
+async function mintTokens(amountToMint: string, tokenAddresses: string[]): Promise<void> {
+  const promises: Array<Promise<string>> = tokenAddresses.map((address: string) => {
+    const contract = getContractOfToken(address);
+    return contract.methods.mint(amountToMint, currentAccount).send({ from: currentAccount })
+  });
+
+  await Promise.all(promises);
+}
+
+function getContractOfToken(tokenAddress: string) {
+  return new web3.eth.Contract((ERC20Mock as any).abi, tokenAddress);
+}
