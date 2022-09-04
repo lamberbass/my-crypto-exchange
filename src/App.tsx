aa5b16688a90fa5f0b6dbc7872661de07e3480cd
Lambros Hasapis <lamberbass@gmail.com>
2022-09-04 12:27:00 +0300

    Add an empty react app.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Crypto Exchange!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
